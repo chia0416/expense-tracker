67697989b40594759a68bbb63f84f9074aa50dbc
chia hung <a0926223478@hotmail.com>
2021-08-12 19:11:17 +0800

    modify recordSeeder

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,8 +1,18 @@
+if(process.env.NODE_ENV !== 'production'){
+  require('dotenv').config()
+}
 const db = require('../../config/mongoose')
 const Record = require('../record')
+const User = require('../user')
+const bcrypt = require('bcryptjs')
 
-db.once('open', () => {
-  Record.create(
+const SEED_USER = {
+  name: 'root',
+  email: 'root@example.com',
+  password: '12345678'
+}
+
+const RecordList = [
     {
       name: '午餐',
       category: '餐飲食品',
@@ -32,9 +42,33 @@ db.once('open', () => {
       category: '其他',
       date: '2021-03-20',
       amount: '10000'
-    }
-  ).then(() => {
-    console.log('recodeSeeder Done!')
-    return db.close
-  })
+    }]
+
+db.once('open', () => {
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(SEED_USER.password, salt))
+    .then(hash => User.create({
+      name: SEED_USER.name,
+      email:SEED_USER.email,
+      password: hash
+    }))
+    .then(user => {
+      const userId = user._id
+      return Promise.all(Array.from(
+        { length: RecordList.length },
+        (_, i) => Record.create({
+          name: RecordList[i].name,
+          category: RecordList[i].category,
+          date: RecordList[i].date,
+          amount: RecordList[i].amount,
+          userId
+        })
+      ))
+    })
+    .then(() => {
+      console.log('done.')
+      process.exit()
+    })
+    .catch(error => console.log(error))
 })
